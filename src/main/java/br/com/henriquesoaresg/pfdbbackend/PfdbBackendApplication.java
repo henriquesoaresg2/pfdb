4c9fde238f912183757d1493083a37a1328d5e87
Henrique Soares <henriquesoaresg2@hotmail.com>
2023-07-15 11:20:42 -0300

    chore: create initial files

---
+++ src/main/java/br/com/henriquesoaresg/pfdbbackend/PfdbBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.henriquesoaresg.pfdbbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfdbBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PfdbBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/henriquesoaresg/pfdbbackend/PfdbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.henriquesoaresg.pfdbbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfdbBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
