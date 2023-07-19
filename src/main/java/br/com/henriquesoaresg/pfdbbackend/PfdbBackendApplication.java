21de626b017e5305729f1bb51d5e831c39fd254e
Henrique Soares <henriquesoaresg2@hotmail.com>
2023-07-18 22:59:44 -0300

    feat: add API informations to Swagger

--- src/main/java/br/com/henriquesoaresg/pfdbbackend/PfdbBackendApplication.java
+++ src/main/java/br/com/henriquesoaresg/pfdbbackend/PfdbBackendApplication.java
@@ -1,11 +1,13 @@
 package br.com.henriquesoaresg.pfdbbackend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "PFDB Api", version = "1", description = "API desenvolvida para demonstrar testes de competência em Spring Boot"))
 public class PfdbBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PfdbBackendApplication.class, args);
     }
